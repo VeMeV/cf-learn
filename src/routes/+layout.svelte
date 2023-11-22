b401d9f6b9b5a3ab95cf2ecbcea17632a24e18f7
Mev <dianameivit@gmail.com>
2023-11-22 12:10:51 +0700

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+    /** @type {import('./$types').LayoutData} */
+    // export let data;
+</script>
+
+
+<slot/>
+
+<ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/about">About</a></li>
+    <li><a href="/contact">Contact</a></li>
+</ul>
